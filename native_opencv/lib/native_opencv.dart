b799627e06444d7b39295fda0a2f3fa9cd28641c
Maxime <maxime.warluzel@dotic.fr>
2024-03-26 14:21:07 +0100

    chore: replace local Native OpenCV dependency

--- native_opencv/lib/native_opencv.dart
+++
@@ -1,7 +0,0 @@
-import 'native_opencv_platform_interface.dart';
-
-class NativeOpencv {
-  Future<String?> getPlatformVersion() {
-    return NativeOpencvPlatform.instance.getPlatformVersion();
-  }
-}
